b0525ff9e6ae4a08dae55f93ed48b3db73aa026e
sofiachami <sofiachami99@gmail.com>
2017-10-31 13:02:27 -0300

    COMMITS

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -2,6 +2,7 @@
 namespace TpFinal;
 include 'Boleto.php';
 
+
 class Tarjeta {
 public $saldo;      //lo pongo public nomas porque sino  falla el test
 public $saldoAcumulado; //lo pongo public nomas porque sino  falla el test
